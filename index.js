9276b98c4cac653409cdeffde59217ca65b612b0
Vitoria Gonçalves <vitoria.goncalvess.gaspar@gmail.com>
2021-06-22 17:44:58 -0300

    Funções

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
-var cinema = "Cine House"
+const catalogo = require("./catalogo");
 
-console.log(cinema)
+function adicionarFilme(titulo, duracao, atores, anoDoLancamento, emCartaz) {
+  const novoFilme = {
+    codigo: catalogo.length + 1,
+    titulo,
+    duracao,
+    atores,
+    anoDoLancamento,
+    emCartaz,
+  };
+
+  catalogo.push(novoFilme);
+  console.log("Filme adicionado com sucesso!");
+}
+
+adicionarFilme("Minha mãe é uma peça", 1, "Paulo Gustavo", 2020, false)
+
+function buscarFilme(codigo) {
+  const filmeProcurado = catalogo[codigo - 1];
+  if (!filmeProcurado) {
+    return console.log("Código não encontrado");
+  }
+  console.log(filmeProcurado);
+}
+
+buscarFilme(2)
+
+function alterarStatusEmCartaz(codigo, novoStatus) {
+  const filmeProcurado = catalogo[codigo - 1];
+
+  if (!filmeProcurado) {
+    return console.log("Código não encontrado");
+  }
+
+  catalogo[codigo - 1].emCartaz = novoStatus;
+
+  console.log("Filme alterado!");
+}
+
+alterarStatusEmCartaz(3, true)
